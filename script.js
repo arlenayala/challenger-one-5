92ee7a6d1cfb433ffa828b91ec21f7be92f239a1
arlenayala <129923851+arlenayala@users.noreply.github.com>
2023-05-10 16:54:54 -0500

    modificaciones generales

--- script.js
+++ script.js
@@ -1,51 +1,35 @@
-<meta charset="UTF-8">
+const textArea = document.querySelector ("#cuadro");
+const mensaje = document.querySelector ("#mensaje");
+/*
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
 
 
 
-
-var cuadro = document.querySelector("textarea"); 
-var encriptar= document.querySelector("button");
-var desencriptar= document.querySelector("button");
-var reiniciar= document.querySelector("button");
-
-
-
-input.focus();
-encriptar.onclick()=Encriptar;
-desencriptar.onclick()=Desencriptar
-reiniciar.onclick()=Reiniciar
-
-
-
-function Encriptar(cadena){
-
- 
+function btnEncriptar() {
+    console.log("Función ejecutada")
+   
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value =textoEncriptado;
 }
 
+function encriptar (stringEncriptada) {
 
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptada=stringEncriptada.tolowerCase();
 
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes (matrizCodigo[i][0])) {
 
-function Desencriptar(){
- 
-}
+            stringEncriptada=stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        } 
+    }
 
-    
-
-
-function Reiniciar () {
-
-
-}
-
-
-
-function saltarlinea () {   
-
-document.write ("<br>" ); 
+    return stringEncriptada
 
 }
-
-
-
 
 
