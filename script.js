aa2e054ba001c049d60a37823ac90f7bd4828ca2
arlenayala <129923851+arlenayala@users.noreply.github.com>
2023-05-10 10:39:34 -0500

    cambie el nombre de challenger_logica por script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+<meta charset="UTF-8">
+
+
+
+
+var cuadro = document.querySelector("textarea"); 
+var encriptar= document.querySelector("button");
+var desencriptar= document.querySelector("button");
+var reiniciar= document.querySelector("button");
+
+
+
+input.focus();
+encriptar.onclick()=Encriptar;
+desencriptar.onclick()=Desencriptar
+reiniciar.onclick()=Reiniciar
+
+
+
+function Encriptar(cadena){
+
+ 
+}
+
+
+
+
+function Desencriptar(){
+ 
+}
+
+    
+
+
+function Reiniciar () {
+
+
+}
+
+
+
+function saltarlinea () {   
+
+document.write ("<br>" ); 
+
+}
+
+
+
+
+
